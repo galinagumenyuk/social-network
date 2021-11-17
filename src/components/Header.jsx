9ccf32581394435b58b07a10400fc11208a62961
galinagumenyuk <gymenyuk.galina@gmail.com>
2021-11-17 17:05:47 +0200

    css modules

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,8 +1,9 @@
 import React from "react";
+import s from'./Header.module.css';
 
 const Header = () => {
-    return (
-        <header className="header">
+  return (
+    <header className={s.header}>
         <img
           src="https://e7.pngegg.com/pngimages/870/903/png-clipart-share-icon-computer-icons-social-networking-service-people-icon-logo-smiley-thumbnail.png"
           alt="logo"
--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -1,29 +1,30 @@
 import React from "react";
+import s from './NavBar.module.css';
 
 const NavBar = () => {
-    return (
-       <nav className="nav">
-        <div>
+  return (
+    <nav className={s.nav}>
+        <div className={s.item}>
           <a href="https://www.youtube.com/watch?v=t6rAzhi3vjQ&list=PLcvhF2Wqh7DNVy1OCUpG3i5lyxyBWhGZ8&index=11">
             Profile
           </a>
         </div>
-        <div>
+        <div className={s.item}>
           <a href="https://www.youtube.com/watch?v=t6rAzhi3vjQ&list=PLcvhF2Wqh7DNVy1OCUpG3i5lyxyBWhGZ8&index=11">
             Messages
           </a>
         </div>
-        <div>
+        <div className={s.item}>
           <a href="https://www.youtube.com/watch?v=t6rAzhi3vjQ&list=PLcvhF2Wqh7DNVy1OCUpG3i5lyxyBWhGZ8&index=11">
             News
           </a>
         </div>
-        <div>
+        <div className={s.item}>
           <a href="https://www.youtube.com/watch?v=t6rAzhi3vjQ&list=PLcvhF2Wqh7DNVy1OCUpG3i5lyxyBWhGZ8&index=11">
             Music
           </a>
         </div>
-        <div>
+        <div className={s.item}>
           <a href="https://www.youtube.com/watch?v=t6rAzhi3vjQ&list=PLcvhF2Wqh7DNVy1OCUpG3i5lyxyBWhGZ8&index=11">
             Settings
           </a>
