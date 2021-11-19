409d6f177a829688fb858db4ddcf4629a23e920f
galinagumenyuk <gymenyuk.galina@gmail.com>
2021-11-19 11:31:50 +0200

    route

--- src/App.js
+++ src/App.js
@@ -1,21 +1,33 @@
 import React from "react";
+import { Route, Routes } from "react-router";
+import { BrowserRouter } from "react-router-dom";
 import "./App.css";
 import Dialogs from "./components/Dialogs/Dialogs";
 import Header from "./components/Header/Header";
 import NavBar from "./components/NavBar/NavBar";
 import Profile from "./components/Profile/Profile";
+import News from "./components/News/News";
+import Music from "./components/Music/Music";
+import Settings from "./components/Settings/Settings";
 
-function App() {
+const App = () => {
   return (
-    <div className="app-wrapper">
-      <Header />
-      <NavBar />
-      <div className="app-wrapper-content">
-        {/* <Profile /> */}
-        <Dialogs />
+    <BrowserRouter>
+      <div className="app-wrapper">
+        <Header />
+        <NavBar />
+        <div className="app-wrapper-content">
+          <Routes>
+            <Route path="/dialogs" element={<Dialogs />} />
+            <Route path="/profile" element={<Profile />} />
+            <Route path="/news" element={<News />} />
+            <Route path="/music" element={<Music />} />
+            <Route path="/settings" element={<Settings />} />
+          </Routes>
+        </div>
       </div>
-    </div>
+    </BrowserRouter>
   );
-}
+};
 
 export default App;
--- src/components/NavBar/NavBar.jsx
+++ src/components/NavBar/NavBar.jsx
@@ -5,27 +5,27 @@ const NavBar = () => {
   return (
     <nav className={s.nav}>
         <div className={s.item}>
-          <a href="https://www.youtube.com/watch?v=t6rAzhi3vjQ&list=PLcvhF2Wqh7DNVy1OCUpG3i5lyxyBWhGZ8&index=11">
+          <a href="/profile">
             Profile
           </a>
         </div>
         <div className={s.item}>
-          <a href="https://www.youtube.com/watch?v=t6rAzhi3vjQ&list=PLcvhF2Wqh7DNVy1OCUpG3i5lyxyBWhGZ8&index=11">
+          <a href="/dialogs">
             Messages
           </a>
         </div>
         <div className={s.item}>
-          <a href="https://www.youtube.com/watch?v=t6rAzhi3vjQ&list=PLcvhF2Wqh7DNVy1OCUpG3i5lyxyBWhGZ8&index=11">
+          <a href="/news">
             News
           </a>
         </div>
         <div className={s.item}>
-          <a href="https://www.youtube.com/watch?v=t6rAzhi3vjQ&list=PLcvhF2Wqh7DNVy1OCUpG3i5lyxyBWhGZ8&index=11">
+          <a href="/music">
             Music
           </a>
         </div>
         <div className={s.item}>
-          <a href="https://www.youtube.com/watch?v=t6rAzhi3vjQ&list=PLcvhF2Wqh7DNVy1OCUpG3i5lyxyBWhGZ8&index=11">
+          <a href="/settings">
             Settings
           </a>
         </div>
