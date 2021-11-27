de1f12f1429da7b14d314cb4824fa781fd622964
galinagumenyuk <gymenyuk.galina@gmail.com>
2021-11-27 18:45:05 +0200

    add dialoges markup, changed styles

--- src/components/Dialogs/Dialogs.jsx
+++ src/components/Dialogs/Dialogs.jsx
@@ -3,8 +3,19 @@ import s from "./Dialogs.module.css";
 
 const Dialogs = () => {
     return (
-        <div>
-            dialogs
+        <div className={s.dialogs}>
+            <div className={s.dialogsItems}>
+                <div className={s.dialog + ' ' +s.active}> Dima</div>
+                <div className={s.dialog}> Petya</div>
+                <div className={s.dialog}> Sasha</div>
+                <div className={s.dialog}> Masha</div>
+            </div>
+            <div className={s.messages}>
+                <div className={s.message}>Hi</div>
+                <div className={s.message}>Hello</div>
+                <div className={s.message}>What's up</div>
+                <div className={s.message}>OK</div>
+            </div>
         </div>
     )
 }
