2b92e54181454df74f698e415c3b85877c2b4a69
galinagumenyuk <gymenyuk.galina@gmail.com>
2021-12-02 20:41:03 +0200

    prettier & eslint

--- src/App.js
+++ src/App.js
@@ -1,6 +1,5 @@
 import React from "react";
 import { Route, Routes } from "react-router";
-import { BrowserRouter } from "react-router-dom";
 import "./App.css";
 import Dialogs from "./components/Dialogs/Dialogs";
 import Header from "./components/Header/Header";
@@ -12,34 +11,32 @@ import Settings from "./components/Settings/Settings";
 
 const App = (props) => {
   return (
-    <BrowserRouter>
-      <div className="app-wrapper">
-        <Header />
-        <NavBar />
-        <div className="app-wrapper-content">
-          <Routes>
-            <Route
-              exact
-              path="/dialogs"
-              element={
-                <Dialogs
-                  dialogs={props.state.dialogsPage.dialogs}
-                  messages={props.state.dialogsPage.messages}
-                />
-              }
-            />
-            <Route
-              exact
-              path="/profile"
-              element={<Profile posts={props.state.profilePage.posts} />}
-            />
-            <Route exact path="/news" element={<News />} />
-            <Route exact path="/music" element={<Music />} />
-            <Route exact path="/settings" element={<Settings />} />
-          </Routes>
-        </div>
+    <div className="app-wrapper">
+      <Header />
+      <NavBar />
+      <div className="app-wrapper-content">
+        <Routes>
+          <Route
+            exact
+            path="/dialogs"
+            element={
+              <Dialogs
+                dialogs={props.state.dialogsPage.dialogs}
+                messages={props.state.dialogsPage.messages}
+              />
+            }
+          />
+          <Route
+            exact
+            path="/profile"
+            element={<Profile posts={props.state.profilePage.posts} />}
+          />
+          <Route exact path="/news" element={<News />} />
+          <Route exact path="/music" element={<Music />} />
+          <Route exact path="/settings" element={<Settings />} />
+        </Routes>
       </div>
-    </BrowserRouter>
+    </div>
   );
 };
 
