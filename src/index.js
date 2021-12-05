a594bb6366c9cfb9ac54d69a34d5884d34fb7b6f
galinagumenyuk <gymenyuk.galina@gmail.com>
2021-12-05 18:14:10 +0200

    almost FLUX

--- src/index.js
+++ src/index.js
@@ -1,19 +1,6 @@
-import React from "react";
-import ReactDOM from "react-dom";
-import "./index.css";
-import App from "./App";
 import reportWebVitals from "./reportWebVitals";
+import { rerenderEntireTree } from "./render";
 import state from "./redux/state";
-import { addPost } from "./redux/state";
-import { BrowserRouter } from "react-router-dom";
 
-ReactDOM.render(
-  <React.StrictMode>
-    <BrowserRouter>
-      <App state={state} addPost={addPost} />
-    </BrowserRouter>
-  </React.StrictMode>,
-  document.getElementById("root")
-);
-
+rerenderEntireTree(state);
 reportWebVitals();
